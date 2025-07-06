6c7ecd03aa972521814c55e450556e3df557db3c
Aethdv <aeth@tuta.io>
2025-07-06 15:53:33 +0200

    Cargo fix

---
+++ rust/target/debug/cpu_stress.d
@@ -0,0 +1,1 @@
+C:\Users\aeth\Desktop\Projects\rustpg\CPU_stress\rust\target\debug\cpu_stress.exe: C:\Users\aeth\Desktop\Projects\rustpg\CPU_stress\rust\main.rs
---
+++ rust/target/debug/deps/cpu_stress.d
@@ -0,0 +1,5 @@
+C:\Users\aeth\Desktop\Projects\rustpg\CPU_stress\rust\target\debug\deps\cpu_stress.exe: main.rs
+
+C:\Users\aeth\Desktop\Projects\rustpg\CPU_stress\rust\target\debug\deps\cpu_stress.d: main.rs
+
+main.rs:
